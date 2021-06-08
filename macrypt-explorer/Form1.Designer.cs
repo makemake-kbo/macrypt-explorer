8a934e533ac1222240b1fc93406a1e0fb875778d
Vukasin Gostovic <vukasin@gostovic.de>
2021-06-08 16:43:24 +0200

    Initial commit

---
+++ macrypt-explorer/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿
+namespace macrypt_explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.addressBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.portBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.getAllBlocksButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.getBlockByIndexButton = new System.Windows.Forms.Button();
+            this.blockIndexBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.postButton = new System.Windows.Forms.Button();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("DejaVu Sans Mono", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "macrypt explorer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("DejaVu Sans Mono", 15.75F);
+            this.label2.Location = new System.Drawing.Point(437, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Node info:";
+            // 
+            // addressBox
+            // 
+            this.addressBox.Location = new System.Drawing.Point(591, 59);
+            this.addressBox.Name = "addressBox";
+            this.addressBox.Size = new System.Drawing.Size(125, 20);
+            this.addressBox.TabIndex = 3;
+            this.addressBox.Text = "localhost";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("DejaVu Sans Mono", 10F);
+            this.label3.Location = new System.Drawing.Point(513, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Address:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("DejaVu Sans Mono", 10F);
+            this.label4.Location = new System.Drawing.Point(529, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Port:";
+            // 
+            // portBox
+            // 
+            this.portBox.Location = new System.Drawing.Point(591, 95);
+            this.portBox.Name = "portBox";
+            this.portBox.Size = new System.Drawing.Size(125, 20);
+            this.portBox.TabIndex = 6;
+            this.portBox.Text = "6475";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("DejaVu Sans Mono", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 24);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "read:";
+            // 
+            // getAllBlocksButton
+            // 
+            this.getAllBlocksButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.getAllBlocksButton.Location = new System.Drawing.Point(16, 133);
+            this.getAllBlocksButton.Name = "getAllBlocksButton";
+            this.getAllBlocksButton.Size = new System.Drawing.Size(86, 23);
+            this.getAllBlocksButton.TabIndex = 8;
+            this.getAllBlocksButton.Text = "Get all blocks";
+            this.getAllBlocksButton.UseVisualStyleBackColor = true;
+            this.getAllBlocksButton.Click += new System.EventHandler(this.getAllBlocksButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(16, 173);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 23);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Get latest";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // getBlockByIndexButton
+            // 
+            this.getBlockByIndexButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.getBlockByIndexButton.Location = new System.Drawing.Point(16, 213);
+            this.getBlockByIndexButton.Name = "getBlockByIndexButton";
+            this.getBlockByIndexButton.Size = new System.Drawing.Size(86, 23);
+            this.getBlockByIndexButton.TabIndex = 10;
+            this.getBlockByIndexButton.Text = "Get block";
+            this.getBlockByIndexButton.UseVisualStyleBackColor = true;
+            this.getBlockByIndexButton.Click += new System.EventHandler(this.getBlockByIndexButton_Click);
+            // 
+            // blockIndexBox
+            // 
+            this.blockIndexBox.Location = new System.Drawing.Point(109, 215);
+            this.blockIndexBox.Name = "blockIndexBox";
+            this.blockIndexBox.Size = new System.Drawing.Size(100, 20);
+            this.blockIndexBox.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("DejaVu Sans Mono", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(13, 262);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 24);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "write:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("DejaVu Sans Mono", 10F);
+            this.label7.Location = new System.Drawing.Point(13, 298);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(48, 16);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "From:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("DejaVu Sans Mono", 10F);
+            this.label8.Location = new System.Drawing.Point(13, 331);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(32, 16);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "To:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("DejaVu Sans Mono", 10F);
+            this.label9.Location = new System.Drawing.Point(14, 364);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 16);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Ammount:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("DejaVu Sans Mono", 10F);
+            this.label10.Location = new System.Drawing.Point(14, 394);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(40, 16);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Fee:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(92, 364);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 18;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(92, 394);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 19;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(92, 331);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 20;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(92, 298);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 21;
+            // 
+            // postButton
+            // 
+            this.postButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.postButton.Location = new System.Drawing.Point(239, 391);
+            this.postButton.Name = "postButton";
+            this.postButton.Size = new System.Drawing.Size(75, 23);
+            this.postButton.TabIndex = 22;
+            this.postButton.Text = "Post";
+            this.postButton.UseVisualStyleBackColor = true;
+            this.postButton.Click += new System.EventHandler(this.postButton_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(441, 173);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(347, 265);
+            this.webBrowser1.TabIndex = 23;
+            this.webBrowser1.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("DejaVu Sans Mono", 15.75F);
+            this.label11.Location = new System.Drawing.Point(445, 146);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(114, 24);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Results:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.postButton);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.blockIndexBox);
+            this.Controls.Add(this.getBlockByIndexButton);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.getAllBlocksButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.portBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.addressBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 489);
+            this.MinimumSize = new System.Drawing.Size(816, 489);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox addressBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox portBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button getAllBlocksButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button getBlockByIndexButton;
+        private System.Windows.Forms.TextBox blockIndexBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button postButton;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ macrypt-explorer/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+
+namespace macrypt_explorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getAllBlocksButton_Click(object sender, EventArgs e)
+        {
+            var nodeAddress = addressBox.Text;
+            var nodePort = portBox.Text;
+            var stringUri = $"http://{nodeAddress}:{nodePort}/api/blocks";
+            Uri uri = new Uri(stringUri);
+
+            webBrowser1.Url = uri;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var nodeAddress = addressBox.Text;
+            var nodePort = portBox.Text;
+            var stringUri = $"http://{nodeAddress}:{nodePort}/api/blocks/latest";
+            Uri uri = new Uri(stringUri);
+
+            webBrowser1.Url = uri;
+        }
+
+        private void getBlockByIndexButton_Click(object sender, EventArgs e)
+        {
+            var nodeAddress = addressBox.Text;
+            var nodePort = portBox.Text;
+            var blockIndex = blockIndexBox.Text;
+            if (String.IsNullOrEmpty(blockIndex) || blockIndex.Any(char.IsLetter))
+            {
+                MessageBox.Show("Block index cant be empty or contain letters!", "error");
+            }
+            else
+            {
+                var stringUri = $"http://{nodeAddress}:{nodePort}/api/blocks/index/{blockIndex}";
+                Uri uri = new Uri(stringUri);
+                webBrowser1.Url = uri;
+            }
+        }
+
+        private async void postButton_Click(object sender, EventArgs e)
+        {
+            var nodeAddress = addressBox.Text;
+            var nodePort = portBox.Text;
+            var from = textBox7.Text;
+            var to = textBox6.Text;
+            var amount = textBox4.Text;
+            var fee = textBox5.Text;
+            if (String.IsNullOrEmpty(from) || String.IsNullOrEmpty(to) || String.IsNullOrEmpty(amount) || String.IsNullOrEmpty(fee)) {
+                MessageBox.Show("Please fill all the fields!", "error");
+            } else
+            {
+                string json = $"{{'From':{from},'To':{to},'Amount':{amount}:,'Fee'{fee}:}}";
+                using (var client = new HttpClient())
+                {
+                    var response = await client.PostAsync(
+                        $"http://{nodeAddress}:{nodePort}/api/add",
+                         new StringContent(json, Encoding.UTF8, "application/json"));
+                }
+            }
+        }
+    }
+}
